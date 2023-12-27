b03364636cb5d2b660185beaaca0ba4956374ea1
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2023-12-27 11:55:11 -0300

    Initial commit

---
+++ BlobDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZUREBLOBSTORAGEAPI
+{
+    public class BlobDTO
+    {
+        public string nome { get; set; }
+        public string Tipo { get; set; }
+        public string Uri { get; set; }
+    }
+}
